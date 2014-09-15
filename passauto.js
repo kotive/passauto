4a9c06d8d312ba7796e365189475031c4c0bf2e4
Hendrik Rust <hendrik@dcinteractive.co.za>
2014-09-15 13:54:37 +0200

    Create passauto.js

---
+++ passauto.js
@@ -0,0 +1,34 @@
+function scorePassword(pass) {
+
+  var score = 0;
+  if (!pass) return score;
+  var letters = new Object();
+  for (var i=0; i<pass.length; i++) {
+      letters[pass[i]] = (letters[pass[i]] || 0) + 1;
+      score += 5.0 / letters[pass[i]];
+  }
+  
+  var variations = {
+      digits: /\d/.test(pass),
+      lower: /[a-z]/.test(pass),
+      upper: /[A-Z]/.test(pass),
+      nonWords: /\W/.test(pass),
+  }
+  
+  variationCount = 0;
+  for (var check in variations) variationCount += (variations[check] == true) ? 1 : 0;
+  score += (variationCount - 1) * 10;
+  
+  return parseInt(score);
+
+}
+
+function checkPassStrength(pass) {
+
+  var score = scorePassword(pass);
+  if (score > 80) return "<span style='color:green; text-align:right;'>Strong password</span>";
+  if (score > 60) return "<span style='color:orange;'>Average password</span>";
+  if (score >= 30) return "<span style='color:red;'>Weak password</span>";
+  return "";
+
+}
