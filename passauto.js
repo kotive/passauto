6ee090f589e8981df5175503376421bd8d4092d8
the-DJDJ <jenkins.daniel.02@gmail.com>
2014-09-17 05:26:37 +0200

    Update passauto.js
    Uses test.txt as i am unable to read the NYT document without errors. test.txt is a saved version

--- passauto.js
+++ passauto.js
@@ -52,3 +52,103 @@ function capitaliseFirstLetter(pass){
 	return pass[0].toUpperCase() + pass.slice(1);
 
 }
+
+function keepLetters(data){
+
+	var out = [];
+
+	for (var i = 0; i < data.length; i++) {
+
+		out[i] = data[i].replace(/[^a-zA-Z0-9]+/g, "");
+	
+	}
+
+	return out;
+
+}
+
+function selectWords(data) {
+				
+	var words = keepLetters(data);
+	var usedWords = [];
+	var password = "";
+	var minimumLength = 3; //can be changed
+
+	while (!isPasswordStrong(password)) {
+				
+		var rand = words[Math.floor(Math.random() * words.length)];
+
+		if(usedWords.indexOf(rand) == -1 && rand.length >= minimumLength){
+
+			var password = password + capitaliseFirstLetter(rand);
+			usedWords.push(rand);
+
+		}
+
+	}
+
+	console.log("(html)    Output:   " + checkPasswordStrength(password));
+	console.log("(boolean) Strong?:  " + isPasswordStrong(password));
+	console.log("(string)  Password: " + password);
+
+	$("#suggested_password").html(password);
+		
+}
+
+function generate(){
+
+	var sections = ["arts",
+			"business",
+			"dining",
+			"fashion",
+			"garden",
+			"health",
+			"magazine",
+			"national",
+			"nyregion",
+			"opinion",
+			"politics",
+			"realestate",
+			"science",
+			"sports",
+			"technology",
+			"travel",					
+			"world"];
+
+	var types =    ["mostemailed",
+			"mostshared",
+			"mostviewed"];
+
+	var key = "c20545b4dc42cab4d83a7ace81ec9ac2:0:69814665";
+	var address = "http://api.nytimes.com/svc/mostpopular/v2/" +
+		types[Math.floor(Math.random() * (types.length - 1))] + "/" +//randomly select one of the different types
+		sections[Math.floor(Math.random() * (sections.length - 1))] + "/1.json?api-key=" + key;
+
+	//address = 'proxy.php?url=' + address;
+
+	$("#suggested_password").html("Loading...");
+		
+	$.ajax({
+		url:		"test.txt",
+		cache:		false,
+		crossOrigin:	true,
+		dataType:	'json',
+		success:	function(data){
+
+			var words = data['results'][0]['abstract'].split(' ');
+						
+			if(words.length == 0){
+
+				generate();
+
+			} else {
+
+				selectWords(words);
+
+			}
+
+		}
+
+	});
+
+}
